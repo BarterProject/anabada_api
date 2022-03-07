67d2f7851d8eaeca6b7ed5a3361e31ba15402ce5
Lim_Yekwang <56215323+rhkd6351@users.noreply.github.com>
2022-03-07 10:45:14 +0900

    Initiate Project

---
+++ src/main/java/com/anabada/anabada_api/AnabadaApiApplication.java
@@ -0,0 +1,13 @@
+package com.anabada.anabada_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnabadaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnabadaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anabada/anabada_api/AnabadaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anabada.anabada_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnabadaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
