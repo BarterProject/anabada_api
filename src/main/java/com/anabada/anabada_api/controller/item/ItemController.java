95814fcb8736af8dc2709d685a54e6a85622f064
Lim_Yekwang <56215323+rhkd6351@users.noreply.github.com>
2022-03-25 12:38:55 +0900

    [implement]: post deal request, get requests, get responses
    GET: /api/user/items/{item-idx}/responses
    GET: /api/user/items/{item-idx}/requests
    POST: /api/user/items/request

--- src/main/java/com/anabada/anabada_api/controller/item/ItemController.java
+++ src/main/java/com/anabada/anabada_api/controller/item/ItemController.java
@@ -1,8 +1,11 @@
 package com.anabada.anabada_api.controller.item;
 
 
+import com.anabada.anabada_api.domain.DealRequestVO;
+import com.anabada.anabada_api.dto.DealRequestDTO;
 import com.anabada.anabada_api.dto.ValidationGroups;
 import com.anabada.anabada_api.dto.item.ItemDTO;
+import com.anabada.anabada_api.service.item.DealRequestService;
 import com.anabada.anabada_api.service.item.ItemFindService;
 import com.anabada.anabada_api.service.item.ItemUpdateService;
 import javassist.NotFoundException;
@@ -15,8 +18,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.security.auth.message.AuthException;
 import javax.transaction.NotSupportedException;
+import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -24,10 +29,12 @@ public class ItemController {
 
     ItemUpdateService itemUpdateService;
     ItemFindService itemFindService;
+    DealRequestService dealRequestService;
 
-    public ItemController(ItemUpdateService itemUpdateService, ItemFindService itemFindService) {
+    public ItemController(ItemUpdateService itemUpdateService, ItemFindService itemFindService, DealRequestService dealRequestService) {
         this.itemUpdateService = itemUpdateService;
         this.itemFindService = itemFindService;
+        this.dealRequestService = dealRequestService;
     }
 
     @PostMapping("/user/items")
@@ -45,16 +52,52 @@ public class ItemController {
     @GetMapping("/user/items")
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     public ResponseEntity<List<ItemDTO>> getMyItems(
-        @RequestParam(value = "option", defaultValue = "registrant") String option
+            @RequestParam(value = "option", defaultValue = "registrant") String option
     ) throws AuthException, NotFoundException {
 
-        if(option.equals("registrant"))
+        if (option.equals("registrant"))
             return new ResponseEntity<>(itemFindService.findByRegistrant(), HttpStatus.OK);
-        else if(option.equals("owner"))
+        else if (option.equals("owner"))
             return new ResponseEntity<>(itemFindService.findByOwner(), HttpStatus.OK);
         else
             throw new NotFoundException("invalid request"); //TODO 예외유형 수정
 
     }
 
+
+    @GetMapping("/user/items/{item-idx}/requests")
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    public ResponseEntity<List<DealRequestDTO>> getRequestsByItem(
+            @PathVariable(value = "item-idx") Long itemIdx
+    ) throws AuthException, NotFoundException {
+        List<DealRequestDTO> dtos = dealRequestService.getRequestsByItem(itemIdx);
+        return new ResponseEntity<>(dtos, HttpStatus.OK);
+    }
+
+    @GetMapping("/user/items/{item-idx}/responses")
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    public ResponseEntity<List<DealRequestDTO>> getResponsesByItem(
+            @PathVariable(value = "item-idx") Long itemIdx
+    ) throws AuthException, NotFoundException {
+        List<DealRequestDTO> dtos = dealRequestService.getResponsesByItem(itemIdx);
+        return new ResponseEntity<>(dtos, HttpStatus.OK);
+    }
+
+    @PostMapping("/user/items/requests")
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    public ResponseEntity<DealRequestDTO> saveRequest(
+            @RequestBody @Validated(ValidationGroups.dealRequestGroup.class) DealRequestDTO dto
+    ) throws NotFoundException, AuthException {
+        DealRequestVO vo = dealRequestService.save(dto);
+        return new ResponseEntity<>(vo.dto(true, true), HttpStatus.OK);
+
+    }
+
 }
+
+
+
+
+
+
+
