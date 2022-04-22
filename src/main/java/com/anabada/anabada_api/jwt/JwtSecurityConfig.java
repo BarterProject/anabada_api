6ef101b72d90d49d13f2c2fadb09aac7e8a1004e
minina0407 <minina@kyonggi.ac.kr>
2022-04-22 23:49:39 +0900

    [implement]feature(delivery/tracking)- 배송조회 : 스마트 택배 OPEN API 이용하여 운송장 번호 조회 기능 구현

--- src/main/java/com/anabada/anabada_api/jwt/JwtSecurityConfig.java
+++ src/main/java/com/anabada/anabada_api/jwt/JwtSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
     private com.anabada.anabada_api.jwt.TokenProvider tokenProvider;
